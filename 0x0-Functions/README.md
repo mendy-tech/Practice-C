0871798c4a3f18341d13ac799cd97d71de2e7b62
Imelda <imeldamendi@gmail.com>
2022-10-20 01:51:16 -0700

    first practise

---
+++ 0x0-Functions/README.md
@@ -0,0 +1,2 @@
+Task 0
+Im creating a function and calling it to perform certain actions.
---
+++ 0x0-Functions/crifunc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - create function and call it.
+ * Return:Always 0;
+ */
+
+void myCricut(void)
+{
+	printf("I love creating\n");
+}
+int main(void)
+{
+	myCricut();
+	myCricut();
+	myCricut();
+	return (0);
+}
