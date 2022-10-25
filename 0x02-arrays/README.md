587b3c0c0bdc361c36ba080aad0e6aa788ff8b18
Imelda <imeldamendi@gmail.com>
2022-10-25 04:34:47 -0700

    run time array

--- 0x02-arrays/README.md
+++ 0x02-arrays/README.md
@@ -1,3 +1,7 @@
 Task 0
 
-This programme prints a one dimensional array.
+This programme prints a one dimensional array at compile time using a sequential for loop.
+
+Task 1
+
+This programme prints  one dimensional array at run time using input from user and a sequential for loop.
---
+++ 0x02-arrays/arrays2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints array at run time.
+ * using a sequential for loop.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int toppers[6];
+	int i;
+
+	printf("Enter the numbers:");
+	for (i = 0; i < 6; i++)
+	{
+		scanf("%d", &toppers[i]);
+		printf("Elements of array are:");
+	}
+	for (i = 0; i < 6; i++)
+	{
+		printf("%d\n", toppers[i]);
+	}
+		return (0);
+}
